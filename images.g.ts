081dcbcf9317ded258432252ff4d03c488f70959
AaronArcia <98153756+AaronArcia@users.noreply.github.com>
2023-07-02 10:34:06 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Mario":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 2 2 2 2 . . . . . . . . 
+. . 2 2 2 2 2 2 2 2 2 . . . . . 
+. . 4 4 4 5 5 4 5 . . . . . . . 
+. 4 5 4 5 5 5 4 5 5 . . . . . . 
+. 4 5 4 4 5 5 5 4 5 5 5 5 . . . 
+. . 4 5 5 5 4 4 4 4 4 4 . . . . 
+. . . 5 5 5 5 5 5 5 5 . . . . . 
+. . . . . 9 . . 8 . . . . . . . 
+. . . . 9 5 8 8 5 8 . . . . . . 
+. . . . 9 8 8 8 8 8 . . . . . . 
+. . . . 9 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . . . e . e e e . . . . . 
+. . . . e e e . e e e e . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
